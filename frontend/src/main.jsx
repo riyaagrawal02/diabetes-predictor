562d799815c35ff2431bf5b6c3d184ddb6014fa5
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2026-02-09 10:45:57 +0530

    add Analytics component for enhanced tracking

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from '../context/AuthContext.jsx'
 import { DiabetesProvider } from '../context/DiabetesContext.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,5 +13,6 @@ createRoot(document.getElementById('root')).render(
         <App />
       </DiabetesProvider>
     </AuthProvider>
+    <Analytics/>
   </StrictMode>,
 )
