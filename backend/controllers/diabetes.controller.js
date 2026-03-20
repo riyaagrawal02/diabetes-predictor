cd329bf8e63337b73269bb3d141e3f6fbf1c3b20
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2026-03-20 15:29:19 +0530

    controller fix

--- backend/controllers/diabetes.controller.js
+++ backend/controllers/diabetes.controller.js
@@ -46,7 +46,7 @@ export const predictDiabetes = async (req, res) => {
 
     
     const response = await axios.post(
-      "http://127.0.0.1:5001/predict",
+      process.env.ML_API_URL +"/predict",
       data,
       {
         headers: {
